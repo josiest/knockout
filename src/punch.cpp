1dc812b6c9a6edd00000a7d882ec12d11a4994af
Josie Thompson <dot.slash.josie@gmail.com>
2021-06-21 17:27:27 -0700

    added basic program structure

--- src/punch.cpp
+++ src/punch.cpp
@@ -1,8 +1,41 @@
 #include "punch.hpp"
 #include <iostream>
+#include <unordered_map>
+
+enum class Command {
+    In, Out, Log, Invalid
+};
 
 int main(int argc, char * argv[])
 {
-    std::cout << paths::project_dir << std::endl;
+    using namespace std;
+    unordered_map<string, Command> argmap = {
+        {"in", Command::In}, {"out", Command::Out}, {"log", Command::Log}
+    };
+
+    // print usage if not used correctly
+    string const usage = "punch <in|out|log>";
+    if (argc < 2) {
+        cout << usage << endl;
+        return 1;
+    }
+
+    // process arguments
+    auto const command_search = argmap.find(argv[1]);
+    auto command = Command::Invalid;
+    if (command_search != argmap.end()) {
+        command = command_search->second;
+    }
+    switch (command) {
+    case Command::In:
+        break;
+    case Command::Out:
+        break;
+    case Command::Log:
+        break;
+    default:
+        cout << usage << endl;
+        return 1;
+    }
     return 0;
 }
